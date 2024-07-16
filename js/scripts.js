f54666d2f662c2503d1b62f76f91d404f20223aa
Almeida-Paulo-dev <almeida.suporte.2013@gmail.com>
2024-07-15 23:03:18 -0300

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o envio padrão do formulário
+    console.log('Formulário submetido!');
     document.getElementById('popup').style.display = 'block'; // Exibe o popup
 });
